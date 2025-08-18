d0328c660aa72e91913ef5c7cd48c29267a19241
HumanEngine <simon.bauske@googlemail.com>
2025-08-18 21:19:12 +0200

    0.1

---
+++ www/visualconfig-card-editor.js
@@ -0,0 +1,48 @@
+class WrapperCardEditor extends HTMLElement {
+  setConfig(config) {
+    this._config = config;
+    this.render();
+  }
+
+  render() {
+    this.innerHTML = `
+      <ha-textfield label="Titel" value="${this._config.title || ''}" id="title"></ha-textfield>
+      <ha-textfield label="Bereich" value="${this._config.area || ''}" id="area"></ha-textfield>
+      <!-- weitere Felder hier -->
+    `;
+  }
+
+  getConfig() {
+    return {
+      title: this.querySelector('#title').value,
+      area: this.querySelector('#area').value,
+      // weitere Optionen
+    };
+  }
+}
+
+customElements.define('better-minimalistic-area-visualconfig-card-editor', WrapperCardEditor);
+class WrapperCardEditor extends HTMLElement {
+  setConfig(config) {
+    this._config = config;
+    this.render();
+  }
+
+  render() {
+    this.innerHTML = `
+      <ha-textfield label="Titel" value="${this._config.title || ''}" id="title"></ha-textfield>
+      <ha-textfield label="Bereich" value="${this._config.area || ''}" id="area"></ha-textfield>
+      <!-- weitere Felder hier -->
+    `;
+  }
+
+  getConfig() {
+    return {
+      title: this.querySelector('#title').value,
+      area: this.querySelector('#area').value,
+      // weitere Optionen
+    };
+  }
+}
+
+customElements.define('better-minimalistic-area-visualconfig-card-editor', WrapperCardEditor);
---
+++ www/visualconfig-card.js
@@ -0,0 +1,21 @@
+class WrapperCard extends HTMLElement {
+  setConfig(config) {
+    this._config = config;
+
+    const card = document.createElement('custom:better-minimalistic-area-card');
+    card.setConfig(config);
+    this.innerHTML = '';
+    this.appendChild(card);
+  }
+
+  set hass(hass) {
+    this._hass = hass;
+    if (this.firstChild) this.firstChild.hass = hass;
+  }
+
+  getCardSize() {
+    return 3;
+  }
+}
+
+customElements.define('better-minimalistic-area-visualconfig-card', WrapperCard);
