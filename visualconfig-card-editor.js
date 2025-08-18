d13dfb3e7c38d7ef3860ee44ba29daef586f9197
HumanEngine <simon.bauske@googlemail.com>
2025-08-18 22:41:12 +0200

    Update visualconfig-card-editor.js

--- visualconfig-card-editor.js
+++ visualconfig-card-editor.js
@@ -22,27 +22,3 @@ class WrapperCardEditor extends HTMLElement {
 }
 
 customElements.define('better-minimalistic-area-visualconfig-card-editor', WrapperCardEditor);
-class WrapperCardEditor extends HTMLElement {
-  setConfig(config) {
-    this._config = config;
-    this.render();
-  }
-
-  render() {
-    this.innerHTML = `
-      <ha-textfield label="Titel" value="${this._config.title || ''}" id="title"></ha-textfield>
-      <ha-textfield label="Bereich" value="${this._config.area || ''}" id="area"></ha-textfield>
-      <!-- weitere Felder hier -->
-    `;
-  }
-
-  getConfig() {
-    return {
-      title: this.querySelector('#title').value,
-      area: this.querySelector('#area').value,
-      // weitere Optionen
-    };
-  }
-}
-
-customElements.define('better-minimalistic-area-visualconfig-card-editor', WrapperCardEditor);
