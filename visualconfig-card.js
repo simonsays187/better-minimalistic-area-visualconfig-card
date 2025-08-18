67c4fcd0d9111ec1e39ff92185713399bba36392
HumanEngine <simon.bauske@googlemail.com>
2025-08-19 01:05:58 +0200

    0.8

--- visualconfig-card.js
+++ visualconfig-card.js
@@ -1,3 +1,26 @@
+class BetterMinimalisticAreaVisualconfigCardEditor extends HTMLElement {
+  setConfig(config) {
+    this._config = config;
+    this.render();
+  }
+
+  render() {
+    this.innerHTML = `
+      <ha-textfield label="Titel" value="${this._config.title || ''}" id="title"></ha-textfield>
+      <ha-textfield label="Bereich" value="${this._config.area || ''}" id="area"></ha-textfield>
+    `;
+  }
+
+  getConfig() {
+    return {
+      title: this.querySelector('#title').value,
+      area: this.querySelector('#area').value,
+    };
+  }
+}
+
+customElements.define('better-minimalistic-area-visualconfig-card-editor', BetterMinimalisticAreaVisualconfigCardEditor);
+
 class BetterMinimalisticAreaVisualconfigCard extends HTMLElement {
   setConfig(config) {
     this._config = config;
@@ -24,27 +47,3 @@ class BetterMinimalisticAreaVisualconfigCard extends HTMLElement {
 }
 
 customElements.define('better-minimalistic-area-visualconfig-card', BetterMinimalisticAreaVisualconfigCard);
-
-
-class BetterMinimalisticAreaVisualconfigCardEditor extends HTMLElement {
-  setConfig(config) {
-    this._config = config;
-    this.render();
-  }
-
-  render() {
-    this.innerHTML = `
-      <ha-textfield label="Titel" value="${this._config.title || ''}" id="title"></ha-textfield>
-      <ha-textfield label="Bereich" value="${this._config.area || ''}" id="area"></ha-textfield>
-    `;
-  }
-
-  getConfig() {
-    return {
-      title: this.querySelector('#title').value,
-      area: this.querySelector('#area').value,
-    };
-  }
-}
-
-customElements.define('better-minimalistic-area-visualconfig-card-editor', BetterMinimalisticAreaVisualconfigCardEditor);
